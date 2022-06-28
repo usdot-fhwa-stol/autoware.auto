4548095865a83077beec69123ffb990f23b32a09
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2022-06-28 16:30:21 -0400

    Update checkout.bash

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -48,5 +48,4 @@ fi
 sudo apt-get update
 sudo apt-get install ros-foxy-nmea-msgs -y
 sudo apt-get install ros-foxy-gps-tools -y
-pwd && cd /opt/carma/ && ls -a 
-cd /opt/carma/src/autoware.ai/carma-develop/lanelet2/lanelet2_python/
+pwd && cd /opt/carma/src && ls -a 
